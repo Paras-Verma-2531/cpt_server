433ab785d412121a22147658979cf26df7828ca2
paras verma <parasverma2531@gmail.com>
2023-11-09 11:50:00 +0530

    User schema modified

--- model/User.js
+++ model/User.js
@@ -1,19 +1,53 @@
+// this file contains the userSchema
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
+//define the Structure for the user
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false, // exclude passwords in the query result
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    //Image will be stored at cloudinary and url will be stored in db
+    avatar: {
+      publicId: String,
+      url: String,
+    },
+    //refers to the users who follows the current user :: me
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId, //fetch the object id from db
+        ref: "user",
+      },
+    ],
+    followings: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user", // used as reference:: should be name of the another schema
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId, //fetch the object id from db
+        ref: "post",
+      },
+    ],
   },
-  password: {
-    type: String,
-    required: true,
-    select: false, // exclude passwords in the query result
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true, // will provide the info about created At and updated At::
+  }
+);
 module.exports = mongoose.model("user", userSchema);
